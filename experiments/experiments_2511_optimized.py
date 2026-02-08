5edc9114b35eabb75a60154145b3dfb116c0e218
Tahirm1213 <Tahir.muhammad@sanofi.com>
2026-02-08 00:20:29 -0500

    Update

--- experiments/experiments_2511_optimized.py
+++ experiments/experiments_2511_optimized.py
@@ -34,7 +34,7 @@ from diffusers import QwenImageEditPlusPipeline, FlowMatchEulerDiscreteScheduler
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 INPUT_DIR = os.path.join(SCRIPT_DIR, "input")
-DEFAULT_OUTPUT_DIR = os.path.join(SCRIPT_DIR, "output", "tablecloths", "optimized_6")
+DEFAULT_OUTPUT_DIR = os.path.join(SCRIPT_DIR, "output", "tablecloths", "optimized_10")
 
 # Base images
 BASE_IMAGES = {
